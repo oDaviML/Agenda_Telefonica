0cb1c8e510a7829316b2c80072c5ea97c989bd04
oDaviML <davimartinslage2005@gmail.com>
2022-10-08 15:58:24 -0300

    Adicionado email e endereço para DTO #3

--- agenda/src/main/java/com/odaviml/DTO.java
+++ agenda/src/main/java/com/odaviml/DTO.java
@@ -1,32 +1,39 @@
 package com.odaviml;
 
 public class DTO {
-    private String nome, telefone, tipo;
+    private String nome, telefone, tipo, email, rua, bairro;
     private int codigo;
 
-
-
-    public DTO(int codigo, String nome, String telefone, String tipo) {
+    public DTO(int codigo, String nome, String telefone, String tipo, String email, String rua, String bairro) {
         this.codigo = codigo;
         this.nome = nome;
         this.telefone = telefone;
         this.tipo = tipo;
+        this.email = email;
+        this.rua = rua;
+        this.bairro = bairro;
     }
 
     public void setCodigo(int codigo) {
         this.codigo = codigo;
     }
-
     public void setNome(String nome) {
         this.nome = nome;
     }
-    
     public void setTelefone(String telefone) {
         this.telefone = telefone;
     }
-
     public void setTipo(String tipo) {
         this.tipo = tipo;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+    public void setRua(String rua) {
+        this.rua = rua;
     }
 
     public int getCodigo() {
@@ -41,11 +48,13 @@ public class DTO {
     public String getTipo() {
         return tipo;
     }
-
-    @Override
-    public String toString() {
-        // TODO Auto-generated method stub
-        return " " + getCodigo() + " " + getNome() + " " + getTelefone() + " " + getTipo();
+    public String getEmail() {
+        return email;
     }
-    
+    public String getBairro() {
+        return bairro;
+    }
+    public String getRua() {
+        return rua;
+    }
 }
