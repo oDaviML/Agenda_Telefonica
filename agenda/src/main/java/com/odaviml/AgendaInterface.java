c4014c4ef4acbf66b772a55fd2f4c8190d966362
oDaviML <davimartinslage2005@gmail.com>
2022-10-08 16:00:37 -0300

    Novas mudanças na interface #8

--- agenda/src/main/java/com/odaviml/AgendaInterface.java
+++ agenda/src/main/java/com/odaviml/AgendaInterface.java
@@ -2,22 +2,23 @@ package com.odaviml;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.ResourceBundle;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 public class AgendaInterface implements Initializable {
+
+    //////////////////////////////////////////////////////////////////
+    //  INPUTS
 
     @FXML
     private Button botaoCadastrar;
@@ -28,10 +29,23 @@ public class AgendaInterface implements Initializable {
     @FXML
     private TextField numeroID;
 
+    @FXML
+    private TextField emailID;
+
+    @FXML
+    private TextField bairroID;
+
+    @FXML
+    private TextField ruaID;
+
     private String[] tps = {"Celular", "Comercial", "Casa"};
 
     @FXML
     private ChoiceBox<String> tipoSelect;;
+
+    //////////////////////////////////////////////////////////////////
+
+
 
     //////////////////////////////////////////////////////////////////
     //  Tabela
@@ -50,9 +64,14 @@ public class AgendaInterface implements Initializable {
     @FXML
     private TableColumn<DTO, String> tipoTabela;
 
-    private List<DTO> listClientes = new ArrayList();
-    private ObservableList<DTO> observableListClientes;
+    @FXML
+    private TableColumn<DTO, String> emailTabela;
 
+    @FXML
+    private TableColumn<DTO, String> ruaTabela;
+
+    @FXML
+    private TableColumn<DTO, String> bairroTabela;
     //
     //////////////////////////////////////////////////////////////////
 
@@ -62,25 +81,39 @@ public class AgendaInterface implements Initializable {
         String nome = nomeID.getText();
         String numero = numeroID.getText();
         String tipo = tipoSelect.getValue();
-        Service.AdicionarContato(nome, numero, tipo);
-        carregarTabela(nome, numero, tipo);
+        String email = emailID.getText();
+        String rua = ruaID.getText();
+        String bairro = bairroID.getText();
+        
+        if (nome.isEmpty() || numero.isEmpty()) {
+            Alert a = new Alert(AlertType.NONE);
+            a.setAlertType(AlertType.WARNING);
+            a.setContentText("Campo Nome/Número não pode estar vazio");
+            a.show();
+        }
+        else {
+            Service.AdicionarContato(nome, numero, tipo, email, rua, bairro);
+            carregarTabela();
+        }
     }
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
+        tipoSelect.setValue("Celular");
         tipoSelect.getItems().addAll(tps);
     }
 
-    public void carregarTabela(String nome, String numero, String tipo) {
+    public void carregarTabela() {
 
         chaveTabela.setCellValueFactory(new PropertyValueFactory<>("codigo"));
         nomeTabela.setCellValueFactory(new PropertyValueFactory<>("nome"));
         numeroTabela.setCellValueFactory(new PropertyValueFactory<>("telefone"));
         tipoTabela.setCellValueFactory(new PropertyValueFactory<>("tipo"));
-
+        emailTabela.setCellValueFactory(new PropertyValueFactory<>("email"));
+        ruaTabela.setCellValueFactory(new PropertyValueFactory<>("rua"));
+        bairroTabela.setCellValueFactory(new PropertyValueFactory<>("bairro"));
 
         contatosTabela.setItems(DAO.getObservableListClientes());
-        System.out.println("Adicionado a Tabela");
     }
 
 }
