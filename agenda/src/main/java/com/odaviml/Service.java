9e62156e947d38e1fdeb5f6fa96c7c3eeb5bc3e3
oDaviML <davimartinslage2005@gmail.com>
2022-10-08 11:54:25 -0300

    Implementado classe Serviço

---
+++ agenda/src/main/java/com/odaviml/Service.java
@@ -0,0 +1,10 @@
+package com.odaviml;
+
+public class Service {
+
+    private Service() {}
+
+    public static DTO AdicionarContato(String nome, String telefone, String tipo){
+        return DAO.inserirContato(nome, telefone, tipo);
+    }
+}
