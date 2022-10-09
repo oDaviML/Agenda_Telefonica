75b1512f0351a6988adfdd286c77a553ba65fa1c
oDaviML <davimartinslage2005@gmail.com>
2022-10-09 14:02:47 -0300

    Implementado Atualizar #5

--- agenda/src/main/java/com/odaviml/Service.java
+++ agenda/src/main/java/com/odaviml/Service.java
@@ -4,11 +4,15 @@ public class Service {
 
     private Service() {}
 
-    public static DTO AdicionarContato(String nome, String telefone, String tipo, String email, String rua, String bairro){
+    public static DTO adicionarContato(String nome, String telefone, String tipo, String email, String rua, String bairro){
         return DAO.inserirContato(nome, telefone, tipo, email, rua, bairro);
     }
 
     public static void removerContato(Integer codigo){
         DAO.removerContato(codigo);
+    }
+
+    public static void editarContato(Integer codigo ,String nome, String telefone, String tipo, String email, String rua, String bairro) {
+        DAO.atualizarContato(codigo, nome, telefone, tipo, email, rua, bairro);
     }
 }
