c93f3e42b72f0e1dcb9faf0c681d28ea82629cd3
oDaviML <davimartinslage2005@gmail.com>
2022-10-09 13:59:36 -0300

    Adicionado ícone #8

--- agenda/src/main/java/com/odaviml/AgendaContatos.java
+++ agenda/src/main/java/com/odaviml/AgendaContatos.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -18,6 +19,7 @@ public class AgendaContatos extends Application {
         stage.setScene(scene);
         stage.setTitle("Agenda Telefonica");
         stage.setResizable(false);
+        stage.getIcons().add(new Image(AgendaContatos.class.getResourceAsStream("contato.png")));
         stage.show();
     }
 
